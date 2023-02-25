d82ca213134c2c2e4bb87522de35137ebb7e47a4
Darren <dali5314@gmail.com>
2023-02-25 12:52:07 -0500

    added sign in firebase auth

--- src/routes/sign-in/sign-in.js
+++ src/routes/sign-in/sign-in.js
@@ -1,7 +1,17 @@
+import {
+  signInWithGooglePopup,
+  createUserDocumentFromAuth,
+} from "../../utils/firebase/firebase";
+
 function SignIn() {
+  const logGoogleUser = async () => {
+    const { user } = await signInWithGooglePopup();
+    createUserDocumentFromAuth(user);
+  };
   return (
     <div>
       <h1>Sign in page</h1>
+      <button onClick={logGoogleUser}>Sign In with Google Popup</button>
     </div>
   );
 }
