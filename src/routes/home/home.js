0af0ab0dfb1541abddb3348b46a26a93a6cb5e70
Darren <dali5314@gmail.com>
2023-02-25 02:40:58 -0500

    set up routes

---
+++ src/routes/home/home.js
@@ -0,0 +1,34 @@
+import Directory from "../../components/directory/directory";
+
+function Home() {
+  const categories = [
+    {
+      id: 1,
+      title: "hats",
+      imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
+    },
+    {
+      id: 2,
+      title: "clothes",
+      imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
+    },
+    {
+      id: 3,
+      title: "booties",
+      imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
+    },
+    {
+      id: 4,
+      title: "womens",
+      imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
+    },
+    {
+      id: 5,
+      title: "mens",
+      imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
+    },
+  ];
+  return <Directory categories={categories} />;
+}
+
+export default Home;
